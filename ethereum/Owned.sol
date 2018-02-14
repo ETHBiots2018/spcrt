214fd9eeeb1dde9f6d059213d5281329535ce6e9
Lukas Tobler <luk4s.tobler@gmail.com>
2018-02-14 15:03:29 +0100

    Finish basic ERC20 token

---
+++ ethereum/Owned.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.19;
+
+/**
+ * Owned contract
+ */
+contract Owned {
+    address public owner;
+    address public newOwner;
+
+    event OwnershipTransferred(address from, address to);
+
+    function Owned() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
+}
+
--- ethereum/SPCRT.sol
+++ ethereum/SPCRT.sol
@@ -2,13 +2,102 @@ pragma solidity ^0.4.19;
 
 import "SafeMath.sol";
 import "ERC20Interface.sol";
+import "Owned.sol";
+
+/*
+ * Contract function to receive approval and execute function in one call
+ */
+contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+}
 
 /**
- * SPCRT Token System
+ * SPCRToken System implementation
  */
-contract SPCRToken is ERC20Interface {
+contract SPCRToken is ERC20Interface, Owned {
+    using SafeMath for uint256;
+
     string public constant name = "SPCRToken";
     string public constant symbol = "SPC";
-    uint8 public constant decimals = 0; // TODO
+    uint8 public constant decimals = 18; // TODO
+    uint256 public _totalSupply;
+    bool mintable = true;
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+
+    /*
+     * ERC20 Interface implementations
+     */
+
+    function totalSupply() public constant returns (uint) {
+        return _totalSupply  - balances[address(0)];
+    }
+
+    function balanceOf(address tokenOwner) public constant returns (uint balance) {
+        return balances[tokenOwner];
+    }
+
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = balances[msg.sender].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+        Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = balances[from].sub(tokens);
+        allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+        Transfer(from, to, tokens);
+        return true;
+    }
+
+    /**
+     * Token owner can approve for `spender` to transferFrom(...) `tokens`
+     * from the token owner's account. The `spender` contract function
+     * `receiveApproval(...)` is then executed
+     */
+    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        Approval(msg.sender, spender, tokens);
+        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+        return true;
+    }
+
+    /**
+     * Mint tokens
+     */
+    function mint(address tokenOwner, uint tokens) public onlyOwner returns (bool success) {
+        require(mintable);
+        balances[tokenOwner] = balances[tokenOwner].add(tokens);
+        _totalSupply = _totalSupply.add(tokens);
+        Transfer(address(0), tokenOwner, tokens);
+        return true;
+    }
+
+    /**
+     * Don't accept ethers
+     */
+    function () public payable {
+        revert();
+    }
+
+    /**
+     * Owner can transfer out any accidentally sent ERC20 tokens
+     */
+    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
+        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+    }
 }
 
